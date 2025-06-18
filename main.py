a09c93f9f6b3029dfafe7d8744df1e21477a7931
HeyHazze <95368310+eodevz@users.noreply.github.com>
2025-06-18 10:05:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+# THIS CAN ACTUALLY BAN YOU FROM SERVERS OR EVEN FROM DISCORD
+# USE AT YOUR OWN RISK.
+
 import pyautogui as keyboardController
 import time as actionController
+from InquirerPy import prompt
 from datetime import datetime
 
 prefix = "$"
 action = "work"
-cooldown = 30
+cooldown = 2
 deposit = True # Set False if you don't want to deposit items after working
+commandLimit = 2 # You can limit it by putting another number, 0 will be infinite.
 
 actionController.sleep(2)
 
+i = 0
 def trigger():
+    global i
     keyboardController.typewrite(f"{prefix}{action}")
     keyboardController.press("enter")
     current_time = datetime.now().strftime("[%H:%M:%S]")
@@ -20,7 +27,20 @@ def trigger():
         keyboardController.press("enter")
         current_time = datetime.now().strftime("[%H:%M:%S]")
         print(f"{current_time} Triggered deposit all command!")
-    
+    i += 1
+    if commandLimit > 0 and i == commandLimit:
+        print(f"{current_time} Command limit reached, stopping.")
+        questions = [
+            {
+                "type": "list",
+                "message": "Press Enter to leave",
+                "choices": ["Leave"],
+                "name": "userInput1"
+            }
+        ]   
+        result = prompt(questions)
+        exit()
+
 
 while True:
     trigger()
